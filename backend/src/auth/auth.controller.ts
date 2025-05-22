30201db844150ba919058c726e40635b787538be
teriTheDesigner <tere1109@stud.kea.dk>
2025-05-22 17:04:54 +0200

    Create auth controller

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { HttpCode, HttpStatus, Post, Body, Controller } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dto/login.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto.email, loginDto.password);
+  }
+}
