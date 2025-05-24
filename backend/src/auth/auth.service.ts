0f619e0998851de1061c50562028db3e0c266143
teriTheDesigner <tere1109@stud.kea.dk>
2025-05-24 21:02:47 +0200

    backend send user data on login

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { instanceToPlain } from 'class-transformer';
+import { User } from '../user.entity';
 
 @Injectable()
 export class AuthService {
@@ -13,7 +15,7 @@ export class AuthService {
   async login(
     email: string,
     password: string,
-  ): Promise<{ access_token: string }> {
+  ): Promise<{ access_token: string; user: Partial<User> }> {
     const user = await this.userService.findByEmail(email);
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
@@ -22,8 +24,11 @@ export class AuthService {
 
     const payload = { sub: user.id, email: user.email };
 
+    const userData = instanceToPlain(user) as Partial<User>;
+
     return {
       access_token: await this.jwtService.signAsync(payload),
+      user: userData,
     };
   }
 }
