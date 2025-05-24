b1edef72829face4b9dee5d176d24ba6cd692793
teriTheDesigner <tere1109@stud.kea.dk>
2025-05-24 21:03:07 +0200

    Add user to store

---
+++ frontend/app/store/user/userSlice.tsx
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface UserState {
+  id?: number;
+  email?: string;
+  name?: string;
+  role?: string;
+  image?: string | null;
+}
+
+const initialState: UserState = {};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser(state, action: PayloadAction<UserState>) {
+      return action.payload;
+    },
+    clearUser() {
+      return {};
+    },
+  },
+});
+
+export const { setUser, clearUser } = userSlice.actions;
+export default userSlice.reducer;
