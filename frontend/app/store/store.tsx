857fe90d13cb8607f36d45872714de41f9865933
teriTheDesigner <tere1109@stud.kea.dk>
2025-05-25 13:20:24 +0200

    Add location reducer to store

--- frontend/app/store/store.tsx
+++ frontend/app/store/store.tsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
 import userReducer from "./user/userSlice";
+import locationReducer from "../locations/LocationsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    locations: locationReducer,
   },
 });
 
